1e5498770604044b9cb091b9370f85e956445830
dekstro <prasanjitmahapatra15@gmail.com>
2024-12-01 09:48:10 +0530

    modified commit 1-dec

--- py_assignment/10fact_without_recur.py
+++ py_assignment/10fact_without_recur.py
@@ -1,7 +1,8 @@
+# function to calculate factorial of a no
 def fact(n):
-    res=1
-    for i in range(1,n+1):
-        res=res*i
+    res=1   # i have taken an initial value 1 stored in res
+    for i in range(1,n+1):  # loop to continue from 1 to n+1 number
+        res=res*i       #result is stored in res var 1*1*2*3*4...
     return res
 n=int(input("Enter a no: "))
 res=fact(n)
--- py_assignment/9annual_sal_func.py
+++ py_assignment/9annual_sal_func.py
@@ -1,11 +1,14 @@
+# function to calculate annual salary
 def annual_sal(msal):
     asal=msal*12
     return asal
+
+# function to calculate provident fund
 def prov_fund():
     msal=float(input("Enter monthly salary: "))
-    asal=annual_sal(msal)
+    asal=annual_sal(msal)    # here i have called annual_sal func to get the annual salary 
     print("Annual salary is %.2f" %asal)
     fund=asal*(12.5/100)
     return fund
-pfund=prov_fund()
+pfund=prov_fund()       # prov_fund func is called 
 print("Provident fund is %.2f" %pfund)
